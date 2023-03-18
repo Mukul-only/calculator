c1afcc0dbc533490a1cd26eb0c9df372118dc61c
unknown <mukul.kushwahaa@gmail.com>
2023-03-18 15:15:40 +0530

    added no select

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ const btns = document.querySelectorAll("button");
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", () => {
     if (btns[i].innerHTML === "=") {
-      input.value = eval(input.value);
+      if (input.value === "") {
+        input.value = "Enter a number";
+      } else {
+        input.value = eval(input.value);
+      }
     } else if (btns[i].innerHTML === "AC") {
       input.value = "";
     } else if (btns[i].innerHTML === "DEL") {
+      if (input.value === "Enter a number") {
+        input.value = "";
+      }
       input.value = input.value.substring(0, input.value.length - 1);
     } else {
+      if (input.value === "Enter a number") {
+        input.value = "";
+      }
       input.value += btns[i].innerHTML;
     }
   });
